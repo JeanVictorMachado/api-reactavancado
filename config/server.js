82a47d3bd4139daa1e9500059f2e2792c1e37bf7
Jean Machado <jeanvictor.arq@gmail.com>
2021-02-01 11:15:58 -0300

    api finalizada

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3fb092fa19e2296c480c7cc1878bf609'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be99bd54-bca8-4049-92fe-c527665cb670'
+};
