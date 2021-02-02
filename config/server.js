175fc8e468769da51c5b7f4f7df45e16aff0e515
Jean Machado <jeanvictor.arq@gmail.com>
2021-02-02 12:06:33 -0300

    resolvendo error favicon.ico

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '3fb092fa19e2296c480c7cc1878bf609'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
